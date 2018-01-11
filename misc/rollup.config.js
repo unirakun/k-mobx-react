ddb9d8ec6f36f8371dde9e86b0d2f20d0412949c
Fabien JUIF <fabien.juif@zenika.com>
2018-01-11 15:03:47 +0100

    :sparkles: add Provider from mobx-react to exports

--- misc/rollup.config.js
+++ misc/rollup.config.js
@@ -2,6 +2,7 @@ import path from 'path'
 import fs from 'fs'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 import uglify from 'rollup-plugin-uglify'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
@@ -14,11 +15,16 @@ export default {
     file: pkg.main,
     format: process.env.FORMAT || 'umd',
   },
-  external: ['fbjs/lib/shallowEqual', 'react', 'mobx'],
+  external: ['fbjs/lib/shallowEqual', 'react', 'mobx', 'react-dom'],
   plugins: [
     babel(),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+      modulesOnly: true,
+    }),
     commonjs({
-      include: 'node_modules/**',
       extensions: ['.js', '.jsx'],
     }),
     uglify(),
@@ -27,5 +33,7 @@ export default {
     react: 'React',
     mobx: 'mobx',
     'fbjs/lib/shallowEqual': 'shallowEqual',
+    'mobx-react': 'mobxReact',
+    'react-dom': 'reactDom',
   },
 }
