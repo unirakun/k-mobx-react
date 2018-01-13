11c0c14d69ff16f632d896626907d0c1aed04f1a
Fabien JUIF <fabien.juif@gmail.com>
2018-01-13 12:17:26 +0100

    :sparkles: add a provider hoc (#6)

---
+++ src/provider.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+import React, { Component } from 'react'
+
+// inspired from mobx-react/Provider :
+// -> https://github.com/mobxjs/mobx-react/blob/master/src/Provider.js
+export default stores => WrappedComponent => class extends Component {
+  static contextTypes = {
+    mobxStores: () => null, // this is to avoid importing prop-types
+  }
+
+  static childContextTypes = {
+    mobxStores: () => null, // this is to avoid importing prop-types
+  }
+
+  getChildContext() {
+    // inner stores are inherit stores and newly injected
+    const mobxStores = {
+      ...this.context.mobxStores,
+      ...stores,
+    }
+
+    return {
+      mobxStores,
+    }
+  }
+
+  render() {
+    return <WrappedComponent {...this.props} />
+  }
+}
